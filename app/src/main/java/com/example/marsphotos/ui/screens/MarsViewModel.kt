69d72ec3fa3370463907cafced9d860aca099f09
AlexPolanco1227 <103541656+AlexPolanco1227@users.noreply.github.com>
2023-09-29 20:22:37 -0500

    Codelab 3
    Cómo cargar y mostrar imágenes de Internet

---
+++ app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
@@ -0,0 +1,59 @@
+package com.example.marsphotos.ui.screens
+
+//Importaciones necesarias
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.marsphotos.MarsPhotosApplication
+import com.example.marsphotos.data.MarsPhotosRepository
+import com.example.marsphotos.model.MarsPhoto
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+//Estado de la interfaz de usuario para la pantalla de inicio
+sealed interface MarsUiState {
+    data class Success(val photos: List<MarsPhoto>) : MarsUiState
+    object Error : MarsUiState
+    object Loading : MarsUiState
+}
+
+class MarsViewModel(private val marsPhotosRepository: MarsPhotosRepository) : ViewModel() {
+    //El estado mutable que almacena el estado de la solicitud más reciente
+    var marsUiState: MarsUiState by mutableStateOf(MarsUiState.Loading)
+        private set
+
+    //Llame a getMarsPhotos() en init para que podamos mostrar el estado inmediatamente.
+    init {
+        getMarsPhotos()
+    }
+    //Funcion para fotos de martes
+    fun getMarsPhotos() {
+        viewModelScope.launch {
+            marsUiState = MarsUiState.Loading
+            marsUiState = try {
+                MarsUiState.Success(marsPhotosRepository.getMarsPhotos())
+            } catch (e: IOException) {
+                MarsUiState.Error
+            } catch (e: HttpException) {
+                MarsUiState.Error
+            }
+        }
+    }
+    //Fábrica para [MarsViewModel] que toma [MarsPhotosRepository] como dependencia
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val application = (this[APPLICATION_KEY] as MarsPhotosApplication)
+                val marsPhotosRepository = application.container.marsPhotosRepository
+                MarsViewModel(marsPhotosRepository = marsPhotosRepository)
+            }
+        }
+    }
+}
